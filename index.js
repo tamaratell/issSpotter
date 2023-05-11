447c682b54eaf4fcdb08ca19c2c2e80123acd225
Tamara Tell <2telltamara@gmail.com>
2023-05-11 19:03:23 -0400

    update index.js to have only fetchFlyoverTimes

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const { fetchFlyoverTimes } = require('./iss');
 
-// fetchIP((error, ip) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(ip);
-//   }
-// });
-
-// fetchCoordsByIP('100000', (error, coordinates) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log(coordinates);
-//   }
-// });
-
 const coordinates = { latitude: 43.653226, longitude: -79.3831843 };
 
 fetchFlyoverTimes(coordinates, (error, flyoverTimes) => {
